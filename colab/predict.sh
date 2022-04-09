9ab7550342f486d9f39aba0d0a1ebeab35c10d93
sebastiangarciaacosta <sebastian.garcia@wearemo.com>
2022-04-09 16:33:09 -0500

    Project restructured

---
+++ colab/predict.sh
@@ -0,0 +1,5 @@
+python Predict.py --data_dir ./data/ \
+               --gpu_id 0 \
+               --test_list ./dataset/valid.txt \
+               --snapshot_dir /content/drive/MyDrive/data_science_projects/landslide4sense-2022/predictions/colab_090422/ \
+               --restore_from /content/drive/MyDrive/data_science_projects/landslide4sense-2022/models/colab_090422/batch500_F1_7259.pth
---
+++ colab/setup.sh
@@ -0,0 +1,3 @@
+mkdir /content/Landslide4Sense/data
+unzip /content/drive/MyDrive/data_science_projects/landslide4sense-2022/data/TrainData.zip -d /content/Landslide4Sense/data/TrainData 
+unzip /content/drive/MyDrive/data_science_projects/landslide4sense-2022/data/ValidData.zip -d /content/Landslide4Sense/data/ValidData
