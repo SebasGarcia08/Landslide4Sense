2dfcfd653dd762bc8e372fbec024ec47478a7304
SebasGarcia08 <segaracos@outlook.com>
2022-05-07 23:29:40 +0000

    Adds changes

--- src/landslide4sense/data/augmentations.py
+++ src/landslide4sense/data/augmentations.py
@@ -5,28 +5,11 @@ h, w = 128, 128
 
 transforms = A.Compose(
     [
-        A.OneOf(
-            [
-                A.RandomSizedCrop(min_max_height=(64, 64), height=h, width=w, p=0.5),
-                A.PadIfNeeded(min_height=h, min_width=w, p=0.5),
-            ],
-            p=1,
-        ),
-        A.VerticalFlip(p=0.5),
         A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
         A.RandomRotate90(p=0.5),
         A.Transpose(p=0.5),
-        A.ShiftScaleRotate(p=0.5),
-        A.OneOf(
-            [
-                A.ElasticTransform(
-                    alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5
-                ),
-                A.GridDistortion(p=0.5),
-                A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=0.5),
-            ],
-            p=0.5,
-        ),
-        A.RandomBrightnessContrast(p=0.5),
+        A.RandomSizedCrop(min_max_height=[96, 96], height= 128, width=128, p=0.5),
+        A.ShiftScaleRotate(p=0.5)
     ]
 )
